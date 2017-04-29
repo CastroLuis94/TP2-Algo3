1c8da31b00c63f52615661fc031fc142bad21e66
Luis Castro <luisitocastro94@hotmail.com>
2017-04-28 23:37:30 -0300

    ej1 le puse los primeros cins representativos de la entrada

---
+++ ej1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <ostream>
+#include <vector>
+
+using namespace std;
+
+
+
+
+
+int main(){
+    int cantProvincias;
+    cin << cantProvincias;
+    
+    
+    int cantRutas;
+    cint << cantRutas;
+    
+    
+    int origen;
+    cin << origen;
+    
+    
+    int destino;
+    cin << destino;
+    
+    
+    int k;
+    cin << k;
+    //k siendo la maxima cantidad de rutas premium que puede haber 
+}

ec06d845210c987a18cf1b32b656b472f4422dc6
CastroLuis94 <castroluis94@hotmail.com>
2017-05-14 01:57:24 -0300

    Update ej1.cpp
    agregada matriz de adyacencia, tipo tupla y arreglado bug al tomar la entrada.

--- ej1.cpp
+++ ej1.cpp
@@ -4,33 +4,25 @@
 
 using namespace std;
 
+struct Tupla{
+    bool hayCamino;
+    int distancia;
+    bool caminoPremium;
+    Tupla(){
+        hayCamino = false;
+        distancia = -1;
+        caminoPremium = false;
+    }
+   Tupla(bool hayC,int d,bool cP){
+        hayCamino = hayC;
+        distancia = d;
+        caminoPremium = cP;
+   }
+};
 
-vector<vector< int > > levantarEntrada(){
-    vector< vector<int> > res;
-    cout << "ingrese la cantidad de ciudades" <<endl;
-    int cantCiudades;
-    cin >> cantCiudades;
-    // n es la candidad de ciudades
-    
-    cout << "ingrese la cantidad de rutas" <<endl;
-    int cantRutas;
-    cin >> cantRutas;
-    //m la cantidad de rutas
-    cout << "¿Cual es la ciudad de origen?" <<endl;
-    int origen;
-    cin >> origen;
-    
-    cout << "¿Cual es la ciudad de destino?" <<endl;
-    int destino;
-    cin >> destino;
-    
-    cout << "¿Cual es la cantidad de rutas premium?" <<endl;
-    int k;
-    cin >> k;
-    //k siendo la maxima cantidad de rutas premium que puede haber 
-
-    cout << "Ingrese las m rutas en formato: numero_ciudad1 numero_ciudad2 es_premium(0 o 1) distancia" <<endl;
+vector<vector< int > > levantarEntrada(int cantRutas){
     int i = 0;
+    vector<vector< int > > res;
     while(i < cantRutas){
         vector<int> camino;
         int ciudad1;
@@ -51,65 +43,16 @@ vector<vector< int > > levantarEntrada(){
     return res;
 }
 
-
-vector<vector< int > > levantarEntrada2(int cantCiudades, int cantRutas){
-    vector< vector<int> > res;
-
-    cout << "¿Cual es la ciudad de origen?" <<endl;
-    int origen;
-    cin >> origen;
-    
-    cout << "¿Cual es la ciudad de destino?" <<endl;
-    int destino;
-    cin >> destino;
-    
-    cout << "¿Cual es la cantidad de rutas premium?" <<endl;
-    int k;
-    cin >> k;
-    //k siendo la maxima cantidad de rutas premium que puede haber 
-
-    cout << "Ingrese las m rutas en formato: numero_ciudad1 numero_ciudad2 es_premium(0 o 1) distancia" <<endl;
-    int i = 0;
-    while(i < cantRutas){
-        vector<int> camino;
-        int ciudad1;
-        cin >> ciudad1;
-        int ciudad2;
-        cin >> ciudad2;
-        int premium;
-        cin >> premium;
-        int distancia;
-        cin >> distancia;
-        camino.push_back(ciudad1);
-        camino.push_back(ciudad2);
-        camino.push_back(premium);
-        camino.push_back(distancia);
-        res.push_back(camino);
-        i++;
-    }
-    return res;
+void mostrar(Tupla t) {
+    cout << "(hC:" << t.hayCamino << ",d:" << t.distancia << ",p:" << t.caminoPremium << ")";  
 }
+    
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-void mostrar(vector<int> vs){
+void mostrar(vector<Tupla> vs){
     cout << "[";
     int i = 0;
     while(i < vs.size()){
-        cout << vs[i];
+        mostrar(vs[i]);
         i++;
         if(i < vs.size()){
             cout << ",";
@@ -118,7 +61,7 @@ void mostrar(vector<int> vs){
     cout << "]" <<endl;
 }
 
-void mostrar(vector<vector<int> > vs){
+void mostrar(vector<vector<Tupla> > vs){
     cout << "[";
     int i = 0;
     while(i < vs.size()){
@@ -130,15 +73,92 @@ void mostrar(vector<vector<int> > vs){
     }
     cout << "]"<<endl;
 }
-int main(){    
-    mostrar(levantarEntrada());
-    int fin1;
-    int fin2;
-    cin >> fin1;
-    cin >> fin2;
-    while((fin1 != -1) and (fin2!= -1)){
-        mostrar(levantarEntrada2(fin1, fin2));
+
+vector<Tupla> crearFila(int M){
+    int i = 0;
+    vector<Tupla> res;
+    while(i < M){
+        res.push_back(Tupla());
+        i++;
     }
-    
+    return res;
+}
+
+vector<vector< Tupla > > matriz(int N, int M){
+    int i = 0;
+    vector<vector<Tupla> > res;
+    vector<Tupla> fila = crearFila(M+1);
+    while(i <= N){
+        res.push_back(fila);
+        i++;
+    }
+    return res;
+}
+
+vector<vector <Tupla> > matrizAdyacencia(int N, int M, vector<vector< int > > vs){
+    vector < vector <Tupla> > res = matriz(N,M);
+    int i = 0;
+    while(i < vs.size()){
+        int ciudad1 = vs[i][0];
+        int ciudad2 = vs[i][1];
+        bool hayCaminoPremium;
+        if (vs[i][2]==1){
+            hayCaminoPremium = true;
+        }else{
+            hayCaminoPremium = false;
+        }
+        int distancia = vs[i][3];
+        res[ciudad1][ciudad2] = Tupla(true,distancia,hayCaminoPremium);
+        res[ciudad2][ciudad1] = Tupla(true,distancia,hayCaminoPremium);
+        // Nota importante, estoy asumiendo que si hay camino de A a B entonces hay camino de B a A y tiene la misma distancia,
+        //capaz estoy flasheando
+        i++;
+    }
+    return res;
+}
+
+
+int main(){
+    while(true){
+        vector< vector<int> > res;
+        cout << "ingrese la cantidad de ciudades" <<endl;
+        int cantCiudades;
+        cin >> cantCiudades;
+        // n es la candidad de ciudades
+        
+        cout << "ingrese la cantidad de rutas" <<endl;
+        int cantRutas;
+        cin >> cantRutas;
+        //m la cantidad de rutas
+        if(cantCiudades == -1 and cantRutas == -1){
+            break;
+        }
+        cout << "¿Cual es la ciudad de origen?" <<endl;
+        int origen;
+        cin >> origen;
+        
+        cout << "¿Cual es la ciudad de destino?" <<endl;
+        int destino;
+        cin >> destino;
+        
+        cout << "¿Cual es la cantidad de rutas premium?" <<endl;
+        int k;
+        cin >> k;
+        //k siendo la maxima cantidad de rutas premium que puede haber 
+        cout << "Ingrese las " << cantRutas << " rutas en formato: numero_ciudad1 numero_ciudad2 es_premium(0 o 1) distancia" <<endl;
+        vector<vector< int > > entrada = levantarEntrada(cantRutas);
+        /* Aclaracion importate.
+        Todas las lineas implementadas para tomar la entrada pensaba ponerlo en una funcion auxiliar grande, pero como tengo que fijarme
+        si me dan -1 -1 lo hice asi, una funcion en C++ tiene solo un tipo de salida, no puedo decir "te doy void si me pasan -1 -1 o
+        un vector de vector si me dan otra cosa." Ahora, este algoritmo solo toma las entradas, despues de eso tendriamos que poner que 
+        tipo de representacion queremos y correr el algoritmo que lo resuelve, si los profes corren nuestra implementacion con un 
+        txt estoy bastante seguro que esto toma las X entradas y las resuelve X veces.
+        Cosa importante, estoy asumiendo que el -1 -1 son los primeros numeros al empezar una nueva entrada y no estan en el medio de
+        otra, caso contrario tendria que hacer un par de cambios pero no creo que sean tan forros(mejor commit ever).
+        */
+        mostrar(matrizAdyacencia(cantCiudades,cantRutas,entrada));
+    }
+  
+    cout << "Fin de la ejecucion del algoritmo." << endl;
     return 0;
 }

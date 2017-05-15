a2c7974d583dd021f34e52621349cb030bc37ec3
Alicia Amalia Alvarez Mon <aalvarez@ws5.labo1>
2017-05-15 20:29:46 -0300

    estupido ej1

--- ej1.cpp
+++ ej1.cpp
@@ -19,115 +19,7 @@ bool noSeteados(bool S[], int N){
 
 }
 
-int minimoNoProcesado(vector <vector<int> > pre, bool S[], int N, int contador[])
-{
-   //habia un min_index
-   int min = INT_MAX;
-   int indice;
-   for (int n = 1; n < N+1; n++){
-        if (S[n] == false && pre[contador[n]][n] <= min){
-            min = pre[contador[n]][n], indice = n;
-        }
-    }
-  
-   return indice;
-}
 
-int minimoNoProcesadosP(int distancias[], bool S[], int N)
-{
-   //habia un min_index
-   int min = INT_MAX;
-   int indice;
-   for (int n = 1; n < N+1; n++){
-        if (S[n] == false && distancias[n] <= min){
-            min = distancias[n], indice = n;
-        }
-    }
-  
-   return indice;
-}
-
-
-
-int Dijkstradirijido(int origen, int destino, vector<vector <Dato> > matriz, int N, int k){
-    //int distancias[N+1];
-    vector<int> distancias(N+1, INT_MAX);
-    vector< vector< int > > pre( k+1, distancias);
-    int contador[N+1];
-    bool S[N+1];
-     for (int i = 1; i < N+1; i++){
-      //  distancias[i] = INT_MAX;
-        S[i] = false;
-        contador[i] = 0;
-    }  
-    
-     pre[contador[origen]][origen] = 0;
-     
-     while(noSeteados(S, N))//decia N-1
-     {
-        
-       int u = minimoNoProcesado(pre, S, N, contador);
-       //cout << u << endl;
-  
-       S[u] = true;
-        
-       
-       for (int n = 1; n < N+1; n++){  
-            if ((!S[n]) && (d(matriz[u][n]) > -1)  && (pre[contador[u]][u]+d(matriz[u][n]) <= pre[contador[n]][n]) && (contador[u] <= k) ){
-                if(premium(matriz[u][n]) and (contador[u] < k) and (k>0) ){
-                    contador[n] = contador[u] + 1;
-                }else{
-                    contador[n] = contador[u];
-                }
-                if((contador[u] < k) or ((contador[u] == k)and (!premium(matriz[u][n])))){                            
-                    pre[contador[n]][n] = pre[contador[u]][u] + d(matriz[u][n]);
-                }
-                if((contador[u] == k) and (premium(matriz[u][n])) and (k>0)){
-                    contador[u]--;
-                }
-            }            
-        }
-    }   
-
-return pre[contador[destino]][destino];
-}
-
-int Dijkstradirijidossinpremium(int origen, int destino, vector<vector <Dato> > matriz, int N, int k){
-    int distancias[N+1];  
-    int contador[N+1];
-    bool S[N+1];
-     for (int i = 1; i < N+1; i++){
-        distancias[i] = INT_MAX;
-        S[i] = false;
-        contador[i] = 0;
-    }  
-
-     distancias[origen] = 0;
-     
-     while(noSeteados(S, N))//decia N-1
-     {
-
-       int u = minimoNoProcesadosP(distancias, S, N);      
-  
-       S[u] = true;
-        
-        
-       for (int n = 1; n < N+1; n++){  
-            if ((!S[n]) && (d(matriz[u][n]) > -1)  && (distancias[u]+d(matriz[u][n]) <= distancias[n]) && (contador[u] <= k) ){
-                 if((contador[u] < k) or ((contador[u] == k)and (!premium(matriz[u][n])))){                            
-                distancias[n] = distancias[u] + d(matriz[u][n]);
-                }
-                if(premium(matriz[u][n])){
-                    contador[n]= contador[u] + 1;
-                }else{
-                    contador[n] = contador[u];
-                }
-            }            
-        }
-    }   
-return distancias[destino];
-
-}
 
 int minimoNoProcesado(int distancias[], bool S[], int N)
 {
@@ -135,7 +27,7 @@ int minimoNoProcesado(int distancias[], bool S[], int N)
    int min = INT_MAX;
    int indice;
    for (int n = 1; n < N+1; n++){
-        if (S[n] == false && distancias[n] <= min){
+        if (S[n] == false && distancias[n] < min){
             min = distancias[n], indice = n;
         }
     }
@@ -144,15 +36,17 @@ int minimoNoProcesado(int distancias[], bool S[], int N)
 }
 
 
-int Dijkstradirijido(int origen, int destino, vector<vector <Dato> > &matriz, int N){
-    int distancias[N];
-    bool S[N];
-     for (int i = 0; i < N; i++){
+void Dijkstradirijido(int origen, int destino, vector<vector <Dato> > &matriz, int N, int distancias[], int antecesor[]){
+    //int distancias[N+1];
+    bool S[N+1];
+    // antecesor[];   
+     for (int i = 1; i < N+1; i++){
         distancias[i] = INT_MAX;
+        antecesor[i] = i;     
         S[i] = false;
     }  
      distancias[origen] = 0;
-     for (int nodos = 0; nodos < N; nodos++)//decia N-1
+     while(noSeteados(S, N))//decia N-1
      {
 
        int u = minimoNoProcesado(distancias, S, N);
@@ -160,15 +54,48 @@ int Dijkstradirijido(int origen, int destino, vector<vector <Dato> > &matriz, in
        S[u] = true;
   
      
-       for (int n = 0; n < N; N++){  
-            if (!S[n] && (d(matriz[u][n+1]) > -1) && (distancias[u]!= INT_MAX) && distancias[u]+d(matriz[u][n+1]) < distancias[n]){
-             distancias[n] = distancias[u] + d(matriz[u][n+1]);
+       for (int n = 1; n < N+1; n++){  
+            if (!S[n] && (d(matriz[u][n]) > -1)  && distancias[u]+d(matriz[u][n]) < distancias[n]){//&& (distancias[u]!= INT_MAX)
+             distancias[n] = distancias[u] + d(matriz[u][n]);
+             antecesor[n] = u;             
             }
         }
     }
-return distancias[destino];
-
 }
+
+void DijkstradirijidoSol(int origen, int destino, vector<vector <Dato> > &matriz, int N, int distancias[], int antecesor[], int cantPremium[]){
+    //int distancias[N+1];
+    bool S[N+1];
+    // antecesor[];   
+     for (int i = 1; i < N+1; i++){
+        distancias[i] = INT_MAX;
+        antecesor[i] = i;     
+        S[i] = false;
+        cantPremium[i] = 0;
+    }  
+     distancias[origen] = 0;
+     while(noSeteados(S, N))//decia N-1
+     {
+
+       int u = minimoNoProcesado(distancias, S, N);
+  
+       S[u] = true;
+  
+     
+       for (int n = 1; n < N+1; n++){  
+            if (!S[n] && (d(matriz[u][n]) > -1)  && distancias[u]+d(matriz[u][n]) < distancias[n]){//&& (distancias[u]!= INT_MAX)
+             distancias[n] = distancias[u] + d(matriz[u][n]);
+             antecesor[n] = u;
+             if(premium(matriz[u][n])){
+             	cantPremium[n] = cantPremium[u]+1;
+             }else{
+             	cantPremium[n] = cantPremium[u];
+             }             
+            }
+        }
+    }
+}
+
 
 int main(){
     while(true){
@@ -211,9 +138,21 @@ int main(){
         Representador rep(cantCiudades,entrada);
         mostrar(rep.matrizAdyacencia());
         mostrar(rep.listaDeIncidencia());
-        mostrar(rep.listaDeAdyacencia());
+        //mostrar(rep.listaDeAdyacencia());
         vector<vector <Dato> > matriz = rep.matrizAdyacencia();
-        cout << "Dio esto: " << Dijkstradirijido(origen, destino ,matriz,cantCiudades, k)<< endl;
+        int distancias[cantCiudades + 1];
+        int antecesor[cantCiudades + 1];
+        int cantPremium[cantCiudades + 1];
+        //bool rutasprem[cantCiudades + 1];
+        DijkstradirijidoSol(origen, destino ,matriz,cantCiudades, distancias, antecesor, cantPremium);
+        if(cantPremium <= k){
+        cout << "Dio esto: " << distancias[destino] << endl;
+    	}else{
+    		return min()
+
+    	}
+        //cout << "Dio esto: " << antecesor[1] <<  antecesor[2] << antecesor[3] << antecesor[4] << endl;
+        //cout << "Dio esto: " << rutasprem[1] <<  rutasprem[2] << rutasprem[3] << rutasprem[4] << endl;
         //cout << "Dio esto: " << Dijkstradirijido(origen, destino ,matriz,cantCiudades)<< endl;
     }
   

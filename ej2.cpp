bc85bfe12d31df2f8a0b8696534b48ffd2371c9d
Santiago Alem <salem@devartis.com>
2017-05-20 11:08:23 -0300

    No esta andando la busqueda binaria todavia , el bellmanFord anda  .

--- ej2.cpp
+++ ej2.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
-#include <ostream>
 #include <vector>
-#include <stdio.h>
-#include "representador.h"
+#include <limits.h>
 
 using namespace std;
 
-vector<int> *inicializarBellman(int n) {
 
-}
 
 // Para bellman ford un grafo va ser un vector de vertices
 struct Eje {
     int origen, destino, peso;
 };
+
 
 void relajar(vector<int> &distancias, Eje grafo) {
     if (distancias[grafo.origen] + grafo.peso < distancias[grafo.destino]) {
@@ -21,8 +18,6 @@ void relajar(vector<int> &distancias, Eje grafo) {
     }
 }
 
-// bellman no se lleva bien con matrices de adyacencia
-// me parece hay que usar otra representacion que me devuelva las aristas
 // A este bellman ford lo tiene que usar una funcion que a partir del grafo de entrada
 // otro grafo que tenga un vertice que tenga grado de salida n (ese tiene que va a ser el origen)
 // y otro grado de entrada n . Asi se asegura que se pueden encontrar ciclos negativos del grafo .
@@ -45,16 +40,69 @@ bool bellmanFord(int origen, vector<Eje> &grafo, int n, int m) {
     return true;
 }
 
-bool bellmanFordMod(vector<Eje> &grafo, int n, int m) {
+bool bellmanFordMod(vector<Eje> &grafo, int n, int m, int c) {
 
 //    Agrego un vertice s t que se conectan a todos los nodos
 
+    vector<Eje> grafo_auxiliar;
+    for (int j = 0; j < m; ++j) {
+        Eje v = {grafo[j].origen, grafo[j].destino, grafo[j].peso - c};
+        grafo_auxiliar.push_back(v);
+    }
+
     for (int i = 0; i < n; ++i) {
-        Eje s = {i, n, 1};
-        Eje t = {n + 1, i, 1};
-        grafo.push_back(s);
-        grafo.push_back(t);
+        Eje s = {n, i, 1};
+        Eje t = {i, n + 1, 1};
+        grafo_auxiliar.push_back(s);
+        grafo_auxiliar.push_back(t);
     }
 
     return bellmanFord(n, grafo, n + 2, m + n);
+}
+
+
+int minimo(vector<Eje> &vector, int m) {
+    int min = INT_MAX;
+    for (int i = 0; i < m; ++i) {
+        if (vector[i].peso < min) { min = vector[i].peso; }
+    }
+    return min;
+}
+
+int maximo(vector<Eje> &vector, int m) {
+    int max = -1;
+    for (int i = 0; i < m; ++i) {
+        if (vector[i].peso > max) { max = vector[i].peso; }
+    }
+    return max;
+}
+
+int maxima_reduccion(vector<Eje> &grafo, int n, int m) {
+    int sup = maximo(grafo, m);
+    int inf = minimo(grafo, m);
+    int c = sup;
+
+    while (inf < sup) {
+        c = ((sup - inf) / 2) + inf;
+        if (bellmanFordMod(grafo, n, m, c)) {
+            inf = c;
+        } else {
+            sup = c;
+        }
+    }
+    return c;
+
+}
+
+int main() {
+    vector<Eje> provincia;
+    Eje ruta_0 = {0, 1, 2};
+    Eje ruta_1 = {1, 2, 1};
+    Eje ruta_2 = {2, 0, 1};
+    provincia.push_back(ruta_0);
+    provincia.push_back(ruta_1);
+    provincia.push_back(ruta_2);
+    int res = maxima_reduccion(provincia, 3, 3);
+
+    cout << res << '\n';
 }

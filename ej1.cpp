0c22787bdc7e07165a9778df8cf3ea2348071832
Alicia Amalia Alvarez Mon <alicia_suerte@hotmail.com>
2017-05-20 18:18:39 -0300

    Ej 1 con matriz de(N+1)*(k+1) . Ej 3 coutea en formato correcto. Comentarios en ej3

--- ej1.cpp
+++ ej1.cpp
@@ -8,45 +8,62 @@
 
 using namespace std;
 
-bool noSeteados(bool S[], int N){
-    bool b= true;
-    for(int i =1; i<N+1; i++){
-        if(S[i] == false){
-            return true;
-        }
-    }
-    return false;
-
-}
 
 
-
-int minimoNoProcesado(int distancias[], bool S[], int N)
+int minimoNoProcesado(int distancias[], bool S[], int N, int k)
 {
    //habia un min_index
    int min = INT_MAX;
    int indice;
-   for (int n = 1; n < N+1; n++){
-        if (S[n] == false && distancias[n] < min){
+   for (int n = (k+1); n < N; n++){  
+       if ((S[n] == false) && (distancias[n] <= min)){        
             min = distancias[n], indice = n;
         }
-    }
-  
+    }  
    return indice;
 }
 
+//NOTA IMPORTANTE:
+//Este es el Djistra usado en la solucion.  
+//N = ((cantCiudades)
+//origen = origen*(k+1)
+void Dijkstradirijido(int origen, vector<vector <int> > &matriz, int N, int k, int distancias[]){
+    //int distancias[N+1];
+    int totalarreglo = ((N+1)*(k+1));
+    bool S[totalarreglo];
+    // antecesor[];   
+     for (int i = (k+1); i < totalarreglo; i++){
+        distancias[i] = INT_MAX;            
+        S[i] = false;
+    }  
+     distancias[origen] = 0;
+     for(int count = (k+1); count < totalarreglo; count++)//decia N-1 totalarreglo-1
+     {
 
-void Dijkstradirijido(int origen, int destino, vector<vector <Dato> > &matriz, int N, int distancias[], int antecesor[]){
+       int u = minimoNoProcesado(distancias, S, totalarreglo, k);
+       //cout << "DEJEN LOS CORES1" << u <<  endl;    
+       S[u] = true;    
+     
+       //cout << "DEJEN LOS CORES" << endl;  
+       for (int n = (k+1); n < totalarreglo; n++){  
+            if (!S[n] && ((matriz[u][n]) < INT_MAX)  && distancias[u]+(matriz[u][n]) < distancias[n]){//&& (distancias[u]!= INT_MAX)
+             distancias[n] = distancias[u] + (matriz[u][n]);                         
+            }
+        }
+    }
+}
+
+/*//Djistra basico
+void Dijkstradirijido(int origen, int destino, vector<vector <int> > &matriz, int N, int distancias[]){
     //int distancias[N+1];
     bool S[N+1];
     // antecesor[];   
      for (int i = 1; i < N+1; i++){
-        distancias[i] = INT_MAX;
-        antecesor[i] = i;     
+        distancias[i] = INT_MAX;            
         S[i] = false;
     }  
      distancias[origen] = 0;
-     while(noSeteados(S, N))//decia N-1
+     for(int count = 1; count < N+1; count++)
      {
 
        int u = minimoNoProcesado(distancias, S, N);
@@ -56,50 +73,17 @@ void Dijkstradirijido(int origen, int destino, vector<vector <Dato> > &matriz, i
      
        for (int n = 1; n < N+1; n++){  
             if (!S[n] && (d(matriz[u][n]) > -1)  && distancias[u]+d(matriz[u][n]) < distancias[n]){//&& (distancias[u]!= INT_MAX)
-             distancias[n] = distancias[u] + d(matriz[u][n]);
-             antecesor[n] = u;             
+             distancias[n] = distancias[u] + d(matriz[u][n]);                         
             }
         }
     }
 }
 
-void DijkstradirijidoSol(int origen, int destino, vector<vector <Dato> > &matriz, int N, int distancias[], int antecesor[], int cantPremium[]){
-    //int distancias[N+1];
-    bool S[N+1];
-    // antecesor[];   
-     for (int i = 1; i < N+1; i++){
-        distancias[i] = INT_MAX;
-        antecesor[i] = i;     
-        S[i] = false;
-        cantPremium[i] = 0;
-    }  
-     distancias[origen] = 0;
-     while(noSeteados(S, N))//decia N-1
-     {
-
-       int u = minimoNoProcesado(distancias, S, N);
-  
-       S[u] = true;
-  
-     
-       for (int n = 1; n < N+1; n++){  
-            if (!S[n] && (d(matriz[u][n]) > -1)  && distancias[u]+d(matriz[u][n]) < distancias[n]){//&& (distancias[u]!= INT_MAX)
-             distancias[n] = distancias[u] + d(matriz[u][n]);
-             antecesor[n] = u;
-             if(premium(matriz[u][n])){
-             	cantPremium[n] = cantPremium[u]+1;
-             }else{
-             	cantPremium[n] = cantPremium[u];
-             }             
-            }
-        }
-    }
-}
+*/
 
 
-
-
-int DijkstraLuis(int origen,int destino,int cantCiudades,int k,vector<vector<Dato> > matrizEntrada){
+/*
+void DijkstraLuis(int origen,int destino,int cantCiudades,int k,vector<vector<Dato> > matrizEntrada){
     vector<vector<bool> > yaPase = matrizBooleana(cantCiudades,k);
     //matriz ciudad por cantidad de caminos premium que podemos usar
     vector <vector< int > > caminosRecorridos = matrizEnteros(cantCiudades,k);
@@ -132,7 +116,7 @@ int DijkstraLuis(int origen,int destino,int cantCiudades,int k,vector<vector<Dat
     }else{
         return minimaDistancia;
     }
-}
+}*/
 
 int main(){
     while(true){
@@ -162,32 +146,28 @@ int main(){
         cin >> k;
         //k siendo la maxima cantidad de rutas premium que puede haber 
         cout << "Ingrese las " << cantRutas << " rutas en formato: numero_ciudad1 numero_ciudad2 es_premium(0 o 1) distancia" <<endl;
-        vector<vector< int > > entrada = levantarEntrada(cantRutas);
-        /* Aclaracion importate.
-        Todas las lineas implementadas para tomar la entrada pensaba ponerlo en una funcion auxiliar grande, pero como tengo que fijarme
-        si me dan -1 -1 lo hice asi, una funcion en C++ tiene solo un tipo de salida, no puedo decir "te doy void si me pasan -1 -1 o
-        un vector de vector si me dan otra cosa." Ahora, este algoritmo solo toma las entradas, despues de eso tendriamos que poner que 
-        tipo de representacion queremos y correr el algoritmo que lo resuelve, si los profes corren nuestra implementacion con un 
-        txt estoy bastante seguro que esto toma las X entradas y las resuelve X veces.
-        Cosa importante, estoy asumiendo que el -1 -1 son los primeros numeros al empezar una nueva entrada y no estan en el medio de
-        otra, caso contrario tendria que hacer un par de cambios pero no creo que sean tan forros(mejor commit ever).
-        */
-        Representador rep(cantCiudades,entrada);
-        //mostrar(rep.matrizAdyacencia());
-        //mostrar(rep.listaDeIncidencia());
-        //mostrar(rep.listaDeAdyacencia());
-        vector<vector <Dato> > matriz = rep.matrizAdyacencia();
-        int distancias[cantCiudades + 1];
-        int antecesor[cantCiudades + 1];
-        int cantPremium[cantCiudades + 1];
-        //bool rutasprem[cantCiudades + 1];
-        cout << DijkstraLuis(origen,destino,cantCiudades,k,matriz) << endl;
-       
-        //cout << "Dio esto: " << antecesor[1] <<  antecesor[2] << antecesor[3] << antecesor[4] << endl;
-        //cout << "Dio esto: " << rutasprem[1] <<  rutasprem[2] << rutasprem[3] << rutasprem[4] << endl;
-        //cout << "Dio esto: " << Dijkstradirijido(origen, destino ,matriz,cantCiudades)<< endl;
-    }
-  
+        vector<vector< int > > entrada = levantarEntrada(cantRutas);//cada vector<int> es de lon = 4, y es una arista.En formato (c1, c2, p, distancia)        
+
+        Representador rep(cantCiudades,entrada);       
+        vector <vector <int> > matriz = rep.matrizConPremiums(k);//Matriz de (n*k) nodos.
+        int distancias[(cantCiudades + 1) * (k+1)]; 
+        Dijkstradirijido((origen*(k+1)), matriz, cantCiudades, k, distancias);
+        int minimaDistancia = INT_MAX;
+
+        int i = (destino*(k+1));
+        while(i < (destino*(k+1)+ (k+1)) ){
+            minimaDistancia = min(minimaDistancia,distancias[i]);
+            i++;
+        }
+        if (minimaDistancia == INT_MAX){
+            minimaDistancia = -1;
+        }     
+        
+        
+        cout << minimaDistancia << endl;
+        //cout << DijkstraLuis(origen,destino,cantCiudades,k,matriz) << endl;
+
+    }  
     cout << "Fin de la ejecucion del algoritmo." << endl;
     return 0;
 }

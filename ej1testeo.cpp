d23498d61b71a868fda0329c20237551ae8f3b00
Alicia Amalia Alvarez Mon <alicia_suerte@hotmail.com>
2017-05-22 12:01:12 -0300

    archivos para medir tiempos, espero

---
+++ ej1testeo.cpp
@@ -0,0 +1,250 @@
+#include <iostream>
+#include <ostream>
+#include <vector>
+#include <stdio.h>
+#include <chrono>
+#include <time.h>
+#include <fstream>
+#include <sstream>
+#include "representador.h"
+
+#define ya chrono::high_resolution_clock::now
+
+
+using namespace std;
+
+
+int minimoNoProcesado(int distancias[], bool S[], int N, int k)
+{
+   //habia un min_index
+   int min = INT_MAX;
+   int indice;
+   for (int n = (k+1); n < N; n++){  
+       if ((S[n] == false) && (distancias[n] <= min)){        
+            min = distancias[n], indice = n;
+        }
+    }  
+   return indice;
+}
+
+//NOTA IMPORTANTE:
+//Este es el Djistra usado en la solucion.  
+//N = ((cantCiudades)
+//origen = origen*(k+1)
+void Dijkstradirijido(int origen, vector<vector <int> > &matriz, int N, int k, int distancias[]){
+    //int distancias[N+1];
+    int totalarreglo = ((N+1)*(k+1));
+    bool S[totalarreglo];
+    // antecesor[];   
+     for (int i = (k+1); i < totalarreglo; i++){
+        distancias[i] = INT_MAX;            
+        S[i] = false;
+    }  
+     distancias[origen] = 0;
+     for(int count = (k+1); count < totalarreglo; count++)//decia N-1 totalarreglo-1
+     {
+
+       int u = minimoNoProcesado(distancias, S, totalarreglo, k);
+       //cout << "DEJEN LOS CORES1" << u <<  endl;    
+       S[u] = true;    
+     
+       //cout << "DEJEN LOS CORES" << endl;  
+       for (int n = (k+1); n < totalarreglo; n++){  
+            if (!S[n] && ((matriz[u][n]) > -1)  && distancias[u]+(matriz[u][n]) < distancias[n]){//&& (distancias[u]!= INT_MAX)
+             distancias[n] = distancias[u] + (matriz[u][n]);                         
+            }
+        }
+    }
+}
+
+/*//Djistra basico
+void Dijkstradirijido(int origen, int destino, vector<vector <int> > &matriz, int N, int distancias[]){
+    //int distancias[N+1];
+    bool S[N+1];
+    // antecesor[];   
+     for (int i = 1; i < N+1; i++){
+        distancias[i] = INT_MAX;            
+        S[i] = false;
+    }  
+     distancias[origen] = 0;
+     for(int count = 1; count < N+1; count++)
+     {
+
+       int u = minimoNoProcesado(distancias, S, N);
+  
+       S[u] = true;
+  
+     
+       for (int n = 1; n < N+1; n++){  
+            if (!S[n] && (d(matriz[u][n]) > -1)  && distancias[u]+d(matriz[u][n]) < distancias[n]){//&& (distancias[u]!= INT_MAX)
+             distancias[n] = distancias[u] + d(matriz[u][n]);                         
+            }
+        }
+    }
+}
+
+*/
+
+
+/*
+void DijkstraLuis(int origen,int destino,int cantCiudades,int k,vector<vector<Dato> > matrizEntrada){
+    vector<vector<bool> > yaPase = matrizBooleana(cantCiudades,k);
+    //matriz ciudad por cantidad de caminos premium que podemos usar
+    vector <vector< int > > caminosRecorridos = matrizEnteros(cantCiudades,k);
+    //matriz ciudad por cantidad de caminosP, distancia.
+    list<tuple<int,int,int,int> > listaLindantes;
+    //Dada una ciudad voy a queres sus adyacente, de donde vino, cantidad de caminos premium usados para llegar hasta ahi y la distancia.
+    listaLindantes.push_front(make_tuple(origen,0,0,0));
+    while(not listaLindantes.empty()){
+        tuple<int,int,int,int> t = listaLindantes.front();
+        listaLindantes.pop_front();
+        int ciudadADondeLlegue = get<0>(t);
+        int cantidadCP = get<2>(t);
+        int distancia = get<3>(t);
+        if (esElMejorCamino(caminosRecorridos,ciudadADondeLlegue,cantidadCP,distancia)){
+            agregarLindantes(matrizEntrada,listaLindantes,t,k);
+            //agregar lindante usa la segunda coordenada para saber de donde vino, asi no las vuelve a contar.
+            caminosRecorridos.at(ciudadADondeLlegue).at(cantidadCP) = distancia;
+            yaPase.at(ciudadADondeLlegue).at(cantidadCP) = true;
+        }
+    }
+   
+    int minimaDistancia = INT_MAX;
+    int i = 0;
+    while(i < caminosRecorridos[destino].size()){
+        minimaDistancia = min(minimaDistancia,caminosRecorridos[destino][i]);
+        i++;
+    }
+    if (minimaDistancia == INT_MAX){
+        return -1;
+    }else{
+        return minimaDistancia;
+    }
+}*/
+
+int main(){
+     char buffer[300]={0};   // lo usaremos para guardar el nombre del fichero
+    
+    
+    sprintf(buffer, "ejercicio1_test1.csv"); // Ahora tenemos en buffer = "holaXX.txt"       
+    
+      
+    ofstream salida;
+    salida.open(buffer, ios::out);
+    
+
+    if(salida.fail()){
+        cerr << "No se abrio bien el archivo" << endl;
+        exit(1);
+    }
+    salida << "Ciudades, Rutas, cantPremium, tiempo1\n";
+
+
+    while(true){
+        vector< vector<int> > res;
+        cout << "ingrese la cantidad de ciudades" <<endl;
+        int cantCiudades;
+        cin >> cantCiudades;
+        // n es la candidad de ciudades
+        
+        cout << "ingrese la cantidad de rutas" <<endl;
+        int cantRutas;
+        cin >> cantRutas;
+        //m la cantidad de rutas
+        if(cantCiudades == -1 and cantRutas == -1){
+            salida.close();
+            break;
+        }
+        cout << "¿Cual es la ciudad de origen?" <<endl;
+        int origen;
+        cin >> origen;
+        
+        cout << "¿Cual es la ciudad de destino?" <<endl;
+        int destino;
+        cin >> destino;
+        
+        cout << "¿Cual es la cantidad de rutas premium?" <<endl;
+        int k;
+        cin >> k;
+        //k siendo la maxima cantidad de rutas premium que puede haber 
+        cout << "Ingrese las " << cantRutas << " rutas en formato: numero_ciudad1 numero_ciudad2 es_premium(0 o 1) distancia" <<endl;
+        vector<vector< int > > entrada = levantarEntrada(cantRutas);//cada vector<int> es de lon = 4, y es una arista.En formato (c1, c2, p, distancia)        
+
+        Representador rep(cantCiudades,entrada);
+               
+        vector <vector <int> > matriz = rep.matrizConPremiums(k);//Matriz de (n*k) nodos.
+       // mostrar(matriz);
+        int distancias[(cantCiudades + 1) * (k+1)];
+        auto start = ya();                       
+        Dijkstradirijido((origen*(k+1)), matriz, cantCiudades, k, distancias);                 
+        int minimaDistancia = INT_MAX;
+        int i = (destino*(k+1));
+        while(i < (destino*(k+1)+ (k+1)) ){
+            if(distancias[i] >= 0){
+            minimaDistancia = min(minimaDistancia,distancias[i]);
+            }
+            i++;
+        }
+        if (minimaDistancia == INT_MAX){
+            minimaDistancia = -1;
+        }
+        auto end = ya();       
+        salida << cantCiudades << ";" << cantRutas << ";" << k << ";"<<chrono::duration_cast<chrono::nanoseconds>(end-start).count() << endl;            
+        cout << minimaDistancia << endl;
+        //cout << DijkstraLuis(origen,destino,cantCiudades,k,matriz) << endl;
+
+    }  
+    cout << "Fin de la ejecucion del algoritmo." << endl;
+    return 0;
+}
+
+
+
+/*
+int main(){
+    vector <int> imprimir;
+    while(true){
+        int cantCiudades;
+        cin >> cantCiudades;
+        // n es la candidad de ciudades
+        
+        int cantRutas;
+        cin >> cantRutas;
+        //m la cantidad de rutas
+        if(cantCiudades == -1 and cantRutas == -1){
+            break;
+        }
+        int origen;
+        cin >> origen;
+        
+        
+        int destino;
+        cin >> destino;
+        
+
+        int k;
+        cin >> k;
+        //k siendo la maxima cantidad de rutas premium que puede haber 
+        vector<vector< int > > entrada = levantarEntrada(cantRutas);
+      
+        Representador rep(cantCiudades,entrada);
+        //mostrar(rep.matrizAdyacencia());
+        //mostrar(rep.listaDeIncidencia());
+        //mostrar(rep.listaDeAdyacencia());
+        vector<vector <Dato> > matriz = rep.matrizAdyacencia();
+        int distancias[cantCiudades + 1];
+        int antecesor[cantCiudades + 1];
+        int cantPremium[cantCiudades + 1];
+        //bool rutasprem[cantCiudades + 1];
+        int aAgregar = DijkstraLuis(origen,destino,cantCiudades,k,matriz);
+        imprimir.push_back(aAgregar);
+       
+        //cout << "Dio esto: " << antecesor[1] <<  antecesor[2] << antecesor[3] << antecesor[4] << endl;
+        //cout << "Dio esto: " << rutasprem[1] <<  rutasprem[2] << rutasprem[3] << rutasprem[4] << endl;
+        //cout << "Dio esto: " << Dijkstradirijido(origen, destino ,matriz,cantCiudades)<< endl;
+    }
+    cout << "resultados = ";
+    mostrar(imprimir);
+    return 0;
+}
+*/
---
+++ ej2testeo.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+#include <vector>
+#include <limits.h>
+#include <chrono>
+#include <time.h>
+#include <fstream>
+#include <sstream>
+#include <stdio.h>
+
+#define ya chrono::high_resolution_clock::now
+
+using namespace std;
+
+
+
+// Para bellman ford un grafo va ser un vector de vertices
+struct Eje {
+    int origen, destino, peso;
+};
+
+
+void relajar(vector<int> &distancias, Eje &grafo) {
+    if (distancias[grafo.origen] + grafo.peso < distancias[grafo.destino]) {
+        distancias[grafo.destino] = distancias[grafo.origen] + grafo.peso;
+    }
+}
+
+// A este bellman ford lo tiene que usar una funcion que a partir del grafo de entrada
+// otro grafo que tenga un vertice que tenga grado de salida n (ese tiene que va a ser el origen)
+// y otro grado de entrada n . Asi se asegura que se pueden encontrar ciclos negativos del grafo .
+bool bellmanFord(int origen, vector<Eje> &grafo, int n, int m) {
+    int i, j;
+    vector<int> distancias;
+    for (i = 0; i < n; ++i) {
+        distancias.push_back(INT_MAX); // Esto no tengo idea de si hacerlo asi , una distancia invalida
+    }
+    distancias[origen] = 0;
+    for (i = 0; i < n - 1; ++i) {
+        for (j = 0; j < m; ++j) {
+            relajar(distancias, grafo[j]);
+        }
+    }
+    for (j = 0; j < m; ++j) {
+        if (distancias[grafo[j].origen] + grafo[j].peso < distancias[grafo[j].destino]) return false;
+    }
+
+    return true;
+}
+
+bool bellmanFordMod(vector<Eje> &grafo, int n, int m, int c) {
+
+    //    Agrego un vertice s t que se conectan a todos los nodos
+
+    vector<Eje> grafo_auxiliar;
+    for (int j = 0; j < m; ++j) {
+        Eje v = {grafo[j].origen, grafo[j].destino, grafo[j].peso - c};
+        grafo_auxiliar.push_back(v);
+    }
+
+    for (int i = 0; i < n; ++i) {
+        Eje s = {n, i, 1};
+        Eje t = {i, n + 1, 1};
+        grafo_auxiliar.push_back(s);
+        grafo_auxiliar.push_back(t);
+    }
+
+    return bellmanFord(n, grafo_auxiliar, n + 2, m + n);
+}
+
+
+int minimo(vector<Eje> &vector, int m) {
+    int min = INT_MAX;
+    for (int i = 0; i < m; ++i) {
+        if (vector[i].peso < min) { min = vector[i].peso; }
+    }
+    return min;
+}
+
+int maximo(vector<Eje> &vector, int m) {
+    int max = -1;
+    for (int i = 0; i < m; ++i) {
+        if (vector[i].peso > max) { max = vector[i].peso; }
+    }
+    return max;
+}
+
+int maximaReduccion(vector<Eje> &grafo, int n, int m) {
+    int sup = maximo(grafo, m) + 1;
+    int inf = minimo(grafo, m);
+    int c = sup;
+
+    while (inf + 1 < sup) {
+        c = ((sup - inf) / 2) + inf;
+        if (bellmanFordMod(grafo, n, m, c)) {
+            inf = c;
+        } else {
+            sup = c;
+        }
+
+    }
+    return inf;
+
+}
+
+int main() {
+    char buffer[300]={0};   // lo usaremos para guardar el nombre del fichero
+    
+    
+    sprintf(buffer, "ejercicio2_test1.csv"); // Ahora tenemos en buffer = "holaXX.txt"       
+    
+      
+    ofstream salida;
+    salida.open(buffer, ios::out);
+    
+
+    if(salida.fail()){
+        cerr << "No se abrio bien el archivo" << endl;
+        exit(1);
+    }
+    salida << "Ciudades, Rutas, tiempo1\n";
+
+    while (true) {
+        vector<Eje> provicia;
+        int cantCiudades;
+        int cantRutas;
+//        cout << "Cantidad de Ciudades y Rutas." << endl;
+        cin >> cantCiudades >> cantRutas;
+        // n es la candidad de ciudades
+        if(cantCiudades == -1 and cantRutas == -1){
+            salida.close();
+            break;
+        }
+        for (int i = 0; i < cantRutas; ++i) {
+//            cout << "Ingrese una ruta valida." << endl;
+            int c1;
+            int c2;
+            int p;
+            cin >> c1 >> c2 >> p;
+            Eje v = {c1, c2, p};
+            provicia.push_back(v);
+        }
+        auto start = ya(); 
+        int res = maximaReduccion(provicia, cantCiudades, cantRutas);
+        auto end = ya();
+        salida << cantCiudades << ";" << cantRutas << ";" <<chrono::duration_cast<chrono::nanoseconds>(end-start).count() << endl;
+        cout << res << endl;
+    }
+//    cout << "Fin de la ejecucion del algoritmo." << endl;
+    return 0;
+}
